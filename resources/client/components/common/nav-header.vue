7caab15445eac74dd426190bd97791ff4e4d5653
The Mimix Company <code@mimix.io>
2020-11-04 20:25:19 +0800

    resources: update nav-header.vue

--- resources/client/components/common/nav-header.vue
+++ resources/client/components/common/nav-header.vue
@@ -43,7 +43,7 @@
           //-       v-list-item-content
           //-         v-list-item-title.body-2.grey--text.text--ligten-2 {{$t('common:header.imagesFiles')}}
           //-         v-list-item-subtitle.overline.grey--text.text--lighten-2 Coming soon
-          v-toolbar-title(:class='{ "mx-3": $vuetify.breakpoint.mdAndUp, "mx-1": $vuetify.breakpoint.smAndDown }')
+          v-toolbar-title(:class='{ "mx-3": $vuetify.breakpoint.mdAndUp, "mx-1": $vuetify.breakpoint.smAndDown }', style='margin-left: 10px !important;')
             span.subheading {{title}}
       v-flex(md4, v-if='$vuetify.breakpoint.mdAndUp')
         v-toolbar.nav-header-inner(color='black', dark, flat)
